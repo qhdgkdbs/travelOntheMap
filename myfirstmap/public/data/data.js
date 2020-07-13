f1eba3aba2a19d1bf34e41cddb2e407e872827e8
qhdgkdbs <qhdgkdbs@gmail.com>
2020-07-13 21:45:32 +0900

    first commit

---
+++ myfirstmap/public/data/data.js
@@ -0,0 +1,27 @@
+var data = [
+    {
+        title : "서울",
+        content : "서울 남산",
+        date : "2020-07-18",
+        img : "https://i.postimg.cc/7hbZdqXX/image.jpg",
+        lat : 37.551009,
+        lng : 126.990782,
+    },
+    {
+        title : "서울",
+        content : "서울 용산",
+        date : "2020-07-20",
+        img : "https://i.postimg.cc/7hbZdqXX/image.jpg",
+        lat : 37.531309,
+        lng : 126.979945,
+    },
+    {
+        title : "숭실대학교",
+        content : "내가 다니는 곳",
+        date : "2020-07-20",
+        img : "https://i.postimg.cc/7hbZdqXX/image.jpg",
+        lat : 37.496464,
+        lng : 126.957471,
+    },
+
+]
