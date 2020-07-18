c2900be51b0f3bb9c9888c4ef6915bd2bc0df77f
qhdgkdbs <qhdgkdbs@gmail.com>
2020-07-18 23:48:10 +0900

    몽고DB연결

--- travelOntheMap/app.js
+++ travelOntheMap/app.js
@@ -1,11 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
+const bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var spotRouter = require('./routes/spot');
 
 var app = express();
 
@@ -14,13 +15,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/register', spotRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ travelOntheMap/models/Spot.js
@@ -0,0 +1,32 @@
+//스키마 설정하는 과정
+const mongoose = require('mongoose')
+
+const spotSchema = mongoose.Schema({
+    spot : {
+        type : String,
+        maxlength : 50
+    },
+    des : {
+        type : String,
+    },
+    moreLink : {
+        type : String,
+        minlength : 1,
+    },
+    picLink : {
+        type : String,
+    },
+    lat : {
+        type : Number,
+    },
+    lng : {
+        type : Number,
+    },
+    updatedAt : {
+        type : Number
+    }
+})
+
+const Spot = mongoose.model('Spot', spotSchema)
+
+module.exports = {Spot}
