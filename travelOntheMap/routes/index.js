9cfde907dd9dcab3dc87b67286f48dd49e5453e8
qhdgkdbs <qhdgkdbs@gmail.com>
2020-07-22 21:38:45 +0900

    로그인 회원가입 기능 작업 시작

--- travelOntheMap/routes/index.js
+++ travelOntheMap/routes/index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const { Spot } = require("../models/Spot");
-const dataSave = require("../public/data/_data")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {  
